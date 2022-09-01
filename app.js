c2a630b6de17ea52665929e63c6cc4d666f6d500
kalin0l <aleksiev2494@gmail.com>
2022-09-01 16:08:01 +0300

    fixed

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/',betRouter);
 app.use('/api/v1',depositRouter);
 
 app.use((req,res,next) => {
-    res.sendFile(__dirname,'public','index.html')
+    res.sendFile(path.resolve(__dirname,'public','index.html'))
 })
 
 
